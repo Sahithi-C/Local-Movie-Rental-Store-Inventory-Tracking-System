a93877f4bc7385ff57b054033886b832c6965bad
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-02-24 21:39:11 -0800

    Update comedy.h

--- comedy.h
+++ comedy.h
@@ -24,20 +24,24 @@ class Comedy : public Movies  // Comedy class inherits from Movie class.
 
     // Comedy()
     // Summary - Default constructor that constructs a Comedy object.
+    // Post-conditions - Constructs a comedy object.
     Comedy();
 
     // Comedy(const Comedy& other)
     // Summary - Copy constructor that copies one Comedy object to another.
+    // Post-conditions - Constructs a comedy object as a copy of ther comedy object.
     Comedy(const Comedy& other);
 
     // ~Comedy()
-    // Summary - Destrcutor.
+    // Summary - Destructor.
+    // Post-conditions - clears any dynamically associated memory.
     ~Comedy();
     
     //------------------------------------------------getter methods---------------------------------------------------------------
     
     // getMovieId() const
     // Summary - Gets the unique identifier for a comedy movie. Implemented from the parent class Movie.
+    // Post-conditions - returns the unique id for a drama movie.
     string getMovieId() const;
 
 };
