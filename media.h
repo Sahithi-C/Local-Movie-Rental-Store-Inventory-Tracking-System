52adc4f393f3b5d848c3594f838e9ff14d7c5e51
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-02-24 20:51:19 -0800

    Update media.h

--- media.h
+++ media.h
@@ -7,7 +7,8 @@
 // Date of Last Modification : 02/24/2024
 
 // Purpose : Defining a media class representing a generic media item. 
-
+//           It represents the mediaType(eg: DVD, VCR) and a content.
+//           The content in our case is a Movie.
 // -------------------------------------------------------------------------------------------------------------------------
 
 #ifndef MEDIA_H
@@ -25,76 +26,56 @@ class Media
     //--------------------------------------------Constructors & Destructor-----------------------------------------------------
 
     // Media()
-    // Summary - Default constructor.
-    // Pre-conditions - None.
-    // Post-conditions - A Media object is initialized with default values.
+    // Summary - Default constructor initializing a Media object with default values.
     Media();
 
     // Media(const Media& other)
     // Summary - Copy constructor.
-    // Pre-conditions - None.
-    // Post-conditions - A Media object is initialized with the data of other Media object.
     Media(const Media& other);
 
     // ~Media()
     // Summary - Destructor.
-    // Pre-conditions - None.
-    // Post-conditions - Cleans any dynamically associated memory.
     ~Media(); 
 
     //----------------------------------------------other member functions---------------------------------------------------------
 
     // reduceStock()
-    // Summary - Reduces the stock of a media item.
-    // Pre-conditions - there should be valid(i.e., stock >= 1) stock available for media item to prcoess the borrow request and decrease stock.
-    // Post-conditions - Decreases the stock of the media item by 1.
+    // Summary - Reduces the stock of a media item by 1 if there's valid stock.
     bool reduceStock();
 
     // increaseStock()
-    // Summary - Increases the stock of media item.
-    // Pre-conditions - None.
-    // Post-conditions - Increases the stock of the media item by 1.
+    // Summary - Increases the stock of media item by 1.
     bool increaseStock();
-    
-    //---------------------------------------------------getter methods------------------------------------------------------------
-
-    // getMediaId() const
-    // Summary - gets the unique id for the media item.
-    // Pre-conditions - ther should be a valid media item to get it's media id.
-    // Post-conditions - Returns the unique id for a media item.
-    int getMediaId() const;
-
-    // getMediaType() const
-    // Summary - gets the type of media item.
-    // Pre-conditions - There should be a valid media of valid type to check for the type of media.
-    // Post-conditions - Returns the type of media item.
-    char getMediaType() const;
-
-    // getStock() const
-    // Summary - gets the available stock count for a particular media.
-    // Pre-conditions - There should be valid item present of valid media type to gets it's stock.
-    // Post-conditions - Returns the stock count for the media item.
-    int getStock() const;
     
     //----------------------------------------------setter methods-------------------------------------------------------
 
-    // setMediaType()
-    // Summary - sets type for a media item.
-    // Pre-conditions - None.
-    // Post-conditions - Type of the media is set for the media item.
-    void setMediaType(char type);
-
     // setStock()
     // Summary - Sets the stock count for a valid media item.
-    // Pre-conditions - There should be a valid media present to be able to set the stock count for the media item.
-    // Post-conditions - Stock count for a valid media item is set.
     void setStock(int stock);
+
+    // setMediaType()
+    // Summary - Sets the type for a media item.
+    void setMediaType(char mediaType);
+
+    //---------------------------------------------------getter methods------------------------------------------------------------
+
+    // getStock() const
+    // Summary - Gets the available stock count for a particular media.
+    int getStock() const;
+
+    // getMediaType() const
+    // Summary - Gets the type of the media item.
+    char getMediaType() const;
+
+    // getMediaId() const
+    // Summary - Abstract class to get the unique identifier of a media item.
+    string getMediaId() const;
+    
 
    private:
     int stock;            // Stock available for the media item.
-    string mediaType;     // Type of media item.
+    char mediaType;       // Type of a media item.
     Movies* movie;        // Pointer to the associated Movies object.
-
 };
 
 #endif
