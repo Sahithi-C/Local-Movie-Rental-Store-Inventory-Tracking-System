61a5d85dec5a6ff2d42f9bf8b0a8574cc1c7f036
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-02-24 21:55:52 -0800

    Update media.h

--- media.h
+++ media.h
@@ -27,48 +27,58 @@ class Media
 
     // Media()
     // Summary - Default constructor initializing a Media object with default values.
+    // Post-conditions - A Media object is initialized with default values.
     Media();
 
     // Media(const Media& other)
     // Summary - Copy constructor.
+    // Post-conditions - A Media object is initialized with the data of other Media object.
     Media(const Media& other);
 
     // ~Media()
     // Summary - Destructor.
+    // Post-conditions - Cleans any dynamically associated memory.
     ~Media(); 
 
     //----------------------------------------------other member functions---------------------------------------------------------
 
     // reduceStock()
     // Summary - Reduces the stock of a media item by 1 if there's valid stock.
+    // Post-conditions - Decreases the stock of the media item by 1.
     bool reduceStock();
 
     // increaseStock()
     // Summary - Increases the stock of media item by 1.
+    // Post-conditions - Increases the stock of the media item by 1.
     bool increaseStock();
     
     //----------------------------------------------setter methods-------------------------------------------------------
 
     // setStock()
     // Summary - Sets the stock count for a valid media item.
+    // Post-conditions - Stock count for a valid media item is set.
     void setStock(int stock);
 
     // setMediaType()
     // Summary - Sets the type for a media item.
+    // Post-conditions - Type of the media is set for the media item.
     void setMediaType(char mediaType);
 
     //---------------------------------------------------getter methods------------------------------------------------------------
 
     // getStock() const
     // Summary - Gets the available stock count for a particular media.
+    // Post-conditions - Returns the stock count for the media item.
     int getStock() const;
 
     // getMediaType() const
     // Summary - Gets the type of the media item.
+    // Post-conditions - Returns the type of media item.
     char getMediaType() const;
 
     // getMediaId() const
     // Summary - Abstract class to get the unique identifier of a media item.
+    // Post-conditions - Returns the unique id for a media item.
     string getMediaId() const;
     
 
