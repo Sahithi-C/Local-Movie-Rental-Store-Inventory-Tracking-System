28f648d5291dcd3c6a9a22e4aae7eee8eba5e045
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-02-24 21:44:31 -0800

    Update parser.h

--- parser.h
+++ parser.h
@@ -28,62 +28,30 @@ class Parser
 
     // Parser()
     // Summary - Default constructor.
-    // Pre-conditions - None.
     // Post-conditions - Constructs a parser object.
     Parser();
 
     // ~Parser()
     // Summary - Destructor.
-    // Pre-conditions - None.
-    // Post-conditions - Clears any dynamically associated memory.
+    // Post-conditions - Clears any dynamically allocated memory.
     ~Parser();
 
     //--------------------------------------------------other member functions-----------------------------------------------------
 
-    // parseInventory(const string& line)
-    // Summary - Parses a string of inventory data and updates the store's inventory.
-    // Pre-conditions - The input data must be properly formatted according to requirements.
-    // Post-conditions - Update's the store's inventory based on the parsed data.
-    void parseInventory(const string& line);
+   
 
-    // parseTransactions(const string& line)
-    // Summary - Parse string of transaction data and updates the reocrds of store's transactions.
+    // parseInventoryFile(const string& line)
+    // Summary - Parses a string of inventory data to build the store's inventory.
     // Pre-conditions - The input data must be properly formatted according to requirements.
-    // Post-conditions - Update's the store's records based on the parsed data.
-    void parseTransactions(const string& line);
+    // Post-conditions - Returns a store object based on the parsed data.
+    Store& parseInventoryFile(const string& line);
     
-    // parseCustomers(const string& line)
-    // Summary - Parse string of customer data and creates corresponding customer objects.
+    // parseCustomersFile(const string& line)
+    // Summary - Parses a string of customer data to create corresponding customer objects.
     // Pre-conditions - The input data must be properly formatted according to requirements.
-    // Post-conditions - Creates customer objects based on parsed data.
-    void parseCustomers(const string& line);
-
-    // parseString(const string& line)
-    // Summary - Parse a string and determine the appropriate action to take based on it's content.
-    // Pre-conditions - The input string must be properly formatted.
-    // Post-conditions - Executes the appropriate action based on parsed data.
-    void parseString(const string& line);
-
-    // createMovie(const vector<string>& movie)
-    // Summary - Creates a movie object based on parsed data.
-    // Pre-conditions - The input vector must contain properly formatted data.
-    // Post-conditions - Returns a pointer to the created movie object.
-    Movies* createMovie(const vector<string>& movie);
-
-    // createMedia(const vector<string>& media)
-    // Summary - Creates a media object based on parsed data.
-    // Pre-conditions - The input vector must contain properly formatted data.
-    // Post-conditions - Returns a pointer to the created media object.
-    Media* createMedia(const vector<string>& media);
-
-    // createCustomer(const vector<string>& customer)
-    // Summary - Creates a customer object based on parsed customer data.
-    // Pre-conditions - The input vector must contain properly formatted data.
-    // Post-conditions - Returns a pointer to the created customer objcet.
-    Customer* createCustomer(const vector<string>& customer);
-
-   private:
-    Store store;           // store object to manage store data.
+    // Post-conditions - Returns a customer object based on the parsed data.
+    Customer& parseCustomersFile(const string& line);
+  
 }
 
 
