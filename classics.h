d2d486b8c02bf5ad1ad3c835c4cf6bc618ec2c62
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-02-24 19:29:19 -0800

    Update classics.h
    Edited comments and a return type.

--- classics.h
+++ classics.h
@@ -6,7 +6,7 @@
 
 // Date of Last Modification : 02/24/2024
 
-// Purpose : Defining a Classics class representing a classic movie.This class inherits from Movie class. 
+// Purpose : Defining a Classics class representing a classic movie. This class inherits from Movie class. 
 
 // -------------------------------------------------------------------------------------------------------------------------
 
@@ -24,59 +24,43 @@ class Classics : public Movies  // Classics class inherits from Movies class
     //--------------------------------------------constructors and destructor-----------------------------------------------------
 
     // Classics()
-    // Summary - default constructor for Classics class.
-    // Pre-conditions - None.
-    // Post-conditions - Constrcts a Classics object.
+    // Summary - Default constructor for Classics class that constructs a Classics object.
     Classics();
 
     // Classics(const Classics& other)
-    // Summary - Copy constructor.
-    // Pre-conditions - None.
-    // Post-conditiond - Constructs a Classics object as a copy of other Classics object.
+    // Summary - Copy constructor that copies one Classics objecct to another.
     Classics(const Classics& other);
 
     // ~Classics()
     // Summary - Destructor.
-    // Pre-consitions - None.
-    // Post-conditions - Clears any dynamically associated memory.
     ~Classics();
 
     //--------------------------------------------Setter methods---------------------------------------------------------------
 
     // setName(const string& firstName, const string& lastName)
-    // Summary - Sets the name of a major actor.
-    // Pre-conditions - None.
-    // Post-conditions - first name and last name of a major actor is set.
+    // Summary - Sets the name of major actor.
     void setName(const string& firstName, const string& lastName);
 
     // setReleaseMonth(int month)
-    // Summary - Sets the release month of a Classic movie.
-    // Pre-conditions - None.
-    // Post-conditions - Release month of a classic movie is set.
-    void setReleaseMonth(int month);
+    // Summary - Sets the release month of a Classics movie.
+    void setReleaseMonth(int releaseMonth);
 
     //--------------------------------------------getter methods------------------------------------------------------------------
 
     // getName() const
-    // Summary - gets the first name and last name of a major actor.
-    // Pre-conditions - None.
-    // Post-conditions - Returns the first name and last name of a major actor.
+    // Summary - Gets the first name and last name of a major actor.
     string getName() const;
 
     // getReleaseMonth() const
-    // Summary - gets the release month of a classic movie.
-    // Pre-conditions - None.
-    // Post-conditions - Returns the release month oa a classic movie.
+    // Summary - Gets the release month of a classic movie.
     int getReleaseMonth() const;
 
     // getMovieId() const
-    // Summary - gets the unique id for a classic movie.
-    // Pre-conditions - a valid movie should exist in classic genre to get it's id.
-    // Post-conditions - Returns the unique id for a classic movie.
-    int getMovieId() const;
+    // Summary - Gets the unique identifier for a classic movie. Implemented from the parent class.
+    string getMovieId() const;
 
    private:
-    int releaseMonth;  // release month oa a classic movie.
+    int releaseMonth;  // release month of a classic movie.
     string lastName;   // last name of the major actor.
     string firstName;  // first name of the major actor.
 };
