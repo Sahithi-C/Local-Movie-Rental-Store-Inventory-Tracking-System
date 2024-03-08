7727bce944cc2fa3ff540e251d2f76d1eefe4c17
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-03-08 01:33:24 -0800

    Create customer.cpp
    Parametrized constructor for initialization added

---
+++ customer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"customer.h"
+
+using namespace std;
+
+Customer::Customer() {
+
+}
+
+Customer::Customer(int customerId, const string& firstName, const string& lastName) {
+    this->customerId = customerId;
+    this->firstName = firstName;
+    this->lastName = lastName;
+}
+
+Customer::~Customer() {
+}
