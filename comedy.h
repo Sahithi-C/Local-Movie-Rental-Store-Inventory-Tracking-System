82b99f6237529fe5d77b0c6aa2fd6bd872cfe139
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-03-09 14:50:35 -0800

    Update comedy.h

--- comedy.h
+++ comedy.h
@@ -1,4 +1,4 @@
-//------------------------------------------------------comedy.h-----------------------------------------------------------
+//------------------------------------------------------Comedy.h-----------------------------------------------------------
 
 // Programmer Name Course Section Number : VG, Jonah Kolar, Sahithi Chimakurthi(CSS 502 A)
 
@@ -24,25 +24,32 @@ class Comedy : public Movies  // Comedy class inherits from Movie class.
 
     // Comedy()
     // Summary - Default constructor.
+    // Pre-conditions - None.
     // Post-conditions - Constructs a comedy object.
     Comedy();
 
+    // Parameterized constructor
+    Comedy(char genreType, const string& directorName, const string& movieTitle, int releaseYear);
+
     // Comedy(const Comedy& other)
     // Summary - Copy constructor.
+    // Pre-conditions - None.
     // Post-conditions - Constructs a comedy object as a copy of ther comedy object.
     Comedy(const Comedy& other);
 
     // ~Comedy()
-    // Summary - Destructor.
-    // Post-conditions - Clears any dynamically associated memory.
+    // Summary - Destrcutor.
+    // Pre-conditions - None.
+    // Post-conditions - clears any dynamically associated memory.
     ~Comedy();
     
     //------------------------------------------------getter methods---------------------------------------------------------------
     
-    // getContentId() const
-    // Summary - Gets the unique identifier for a comedy movie. Implemented from the parent class Movie.
-    // Post-conditions - Returns the unique identifier for a comedy movie.
-    string getContentId() const;
+    // getMovieId() const
+    // Summary - gets the unique id for a comedy movie.
+    // Pre-conditions - There should be a valid movie in movies of drama genre type.
+    // Post-conditions - returns the unique id for a drama movie.
+    int getMovieId() const;
 
 };
 
