a002b9f41511efe8e922ef0bde94fae8b1d649c9
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-03-09 14:51:45 -0800

    Create classics.cpp

---
+++ classics.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include "classics.h"
+
+using namespace std;
+
+Classics::Classics(){
+}
+
+Classics::Classics(char genreType, const string &directorName,
+                   const string &movieTitle, int releaseYear,
+                   const string &actorFirstName, const string &actorLastname,
+                   int releaseMonth)
+    : Movies(genreType, directorName), Content(movieTitle, releaseYear)
+{
+
+    this->firstName = actorFirstName;
+    this->lastName = actorLastname;
+    this->releaseMonth = releaseMonth;
+}
+
+Classics::~Classics() {
+}
