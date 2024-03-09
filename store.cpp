4e82b8ef79de0fc0c87875a3c942fae573a26e47
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-03-08 19:42:29 -0800

    Update store.cpp
    buildInventory

--- store.cpp
+++ store.cpp
@@ -11,6 +11,11 @@ Store::~Store() {
 
 }
 
+void Store::buildInventory(const list<inventoryData> &inventoryList) {
+
+    inventory.populateInventory(inventoryList);
+}
+
 void Store::setCustomers(Map<int, Customer>& customerMap) {
 
     this->customerMap = customerMap;
