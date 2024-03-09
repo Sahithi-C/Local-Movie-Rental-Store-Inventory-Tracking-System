54f3731ce44f8b5dab62b3531a9940c6ed55c89b
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-03-08 18:49:18 -0800

    Update parser.cpp
    Added parseInventoryFile

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "parser.h"
 
+
 using namespace std;
 
 Parser::Parser() {
@@ -11,7 +12,7 @@ Parser::~Parser() {
 
 }
 
-Map<string, Customer> Parser::parseCustomersFile(const string fileName) {
+Map<int, Customer> Parser::parseCustomersFile(const string fileName) {
 
     Map<int, Customer> customerMap;
 
@@ -19,9 +20,10 @@ Map<string, Customer> Parser::parseCustomersFile(const string fileName) {
     inputFile.open(fileName);
     if(!inputFile) {
         cout << "File could not be opened" << endl;
-        reutrn customerMap;
+        return customerMap;
     }
 
+    
     string line;
     while(getline(inputFile, line)) {
 
@@ -43,4 +45,68 @@ Map<string, Customer> Parser::parseCustomersFile(const string fileName) {
     inputFile.close();
 
     return customerMap;
+}
+
+
+list<inventoryData> Parser::parseInventoryFile(const string fileName) {
+
+    list<inventoryData> inventoryList;
+
+    ifstream inputFile;
+    inputFile.open(fileName);
+    if(!inputFile) {
+        cout << "File could not be opened" << endl;
+        return inventoryList;
+    }
+
+    string line;
+    while(getline(inputFile, line)) {
+        stringstream ss(line);
+        inventoryData data;
+
+        char genre;
+        ss >> genre;
+
+        if (genre != 'C' && genre != 'D' && genre != 'F') {
+            cout << "Invalid genre type" << endl;
+            continue;                       // Move on to the next line
+        }
+
+        if(genre == 'F' || genre == 'D') {
+
+            data.genreType = genre;
+
+            ss.ignore();
+            ss >> data.stock;
+
+            ss.ignore();
+            getline(ss, data.directorName, ',');
+
+            getline(ss, data.movieTitle, ',');
+
+            ss >> data.releaseYear;
+        }
+        else if (genre == 'C') {
+
+            data.genreType = genre;
+
+            ss.ignore();
+            ss >> data.stock;
+
+            ss.ignore();
+            getline(ss, data.directorName, ',');
+
+            getline(ss, data.movieTitle, ',');
+            ss >> data.actorFirstName >> data.actorLastname >> data.releaseMonth >> data.releaseYear;
+        }
+
+        // Add the parsed data to the inventory list
+        inventoryList.push_back(data);
+
+        if(inputFile.eof())
+        break;
+    }
+
+    inputFile.close();
+    return inventoryList;
 }
