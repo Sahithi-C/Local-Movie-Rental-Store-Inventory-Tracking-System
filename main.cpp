b0f73f6a5b6449e851c78833661543a344e08db8
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-08 16:16:07 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,45 @@ int main() {
     Parser parser;
     Store store;
     
-    //Creating customer list
-    store.setCustomers(parser.parseCustomersFile("data4customers.txt"));
+    // //Creating customer list
+    // store.setCustomers(parser.parseCustomersFile("data4customers.txt"));
 
+    ifstream inventoryFile("data4movies.txt");
+    if(inventoryFile.is_open())
+    {
+        Map<string, Media> inventoryData = parser.parserInventoryFile(inventoryFile);
+        inventoryFile.close();
+
+        store.buildInventory(inventoryData);
+    }
+    else
+    {
+        cout << "unable to open inventory file" << endl;
+    }
+
+    ifstream customerFile("data4customers.txt");
+    if(customerFile.is_open())
+    {
+        Map<string, Customer> customerData = parser.parseCustomerFile(customerFile);
+        customerFile.close();
+
+        store.setCustomers(customerData);
+    }
+    else
+    {
+        cout << "unable to open customer file" << endl;
+    }
+
+    ifstream commandsFile("data4commands.txt");
+    if(commandsFile.is_open())
+    {
+        parser.processCommands(commandFile, store);
+        commandFile.close();
+    }
+    else
+    {
+        cout << "unable to open commandFile" << endl;
+    }
 
     return 0;
 }
