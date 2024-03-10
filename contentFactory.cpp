60a939e991dceac5cdcc6746c0c8299511f64ee9
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 16:07:17 -0700

    Update contentFactory.cpp

--- contentFactory.cpp
+++ contentFactory.cpp
@@ -6,14 +6,31 @@
 
 using namespace std;
 
+// ContentFactory::ContentFactory()
+// Summary - default constructor for the ContentFactory class.
+// Pre-conditions - None.
+// Post-conditions - An instance of the ContentFactory class is created.
 ContentFactory::ContentFactory() {
 
 }
 
+// ~ContentFactory()
+// Summary - destructor for the ContentFactory class.
+
+// Pre-conditions - None.
+
+// Post-conditions - Resources held by the ContentFactory object are released upon destruction.
 ContentFactory::~ContentFactory() {
 
 }
 
+// createContent(const InventoryData &data)
+// Summary - creates a Content object based on the InventoryData provided.
+// Pre-conditions - The InventoryData object 'data' must contain valid information about the content to be created.
+//                - The genreType of the InventoryData object must be one of 'F', 'D', or 'C', indicating Comedy, Drama, or Classics respectively.
+// Post-conditions - If the genreType is 'F', a Comedy object is created with the provided data and returned. - If the genreType is 'D', a Drama object is created with the provided data and returned.
+//                 - If the genreType is 'C', a Classics object is created with the provided data and returned.
+//                 - The returned object's ownership is transferred to the caller, and it is responsible for managing its memory.
 Content& ContentFactory::createContent(const InventoryData &data) {
    
     if(data.genreType == 'F') {
