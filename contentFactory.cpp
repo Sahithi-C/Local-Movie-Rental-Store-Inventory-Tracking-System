63b7a136d950db7fd07ef3172bdbf4cfe56a9abb
vgsakura <161179663+vgsakura@users.noreply.github.com>
2024-03-10 16:01:50 -0700

    Update contentFactory.cpp
    Added overloaded method for createContent

--- contentFactory.cpp
+++ contentFactory.cpp
@@ -1,18 +1,11 @@
 #include<iostream>
 #include"contentFactory.h"
-#include "comedy.h"
-#include "drama.h"
-#include "classics.h"
 
 using namespace std;
 
-ContentFactory::ContentFactory() {
+ContentFactory::ContentFactory() {}
 
-}
-
-ContentFactory::~ContentFactory() {
-
-}
+ContentFactory::~ContentFactory() {}
 
 Content& ContentFactory::createContent(const InventoryData &data) {
    
@@ -28,5 +21,18 @@ Content& ContentFactory::createContent(const InventoryData &data) {
     }
 }
 
+Content& ContentFactory::createContent(const CommandData &data) {
+   
+    if(data.genreType == 'F') {
+        return Comedy comedyObject(data.genreType, data.directorName, data.movieTitle, data.releseYear);
+    }
+    else if(data.genreType == 'D') {
+        return  Drama dramaObject(data.genreType, data.directorName, data.movieTitle, data.releaseYear);
+    }
+    else if(data.genreType == 'C') {
+        return Classics classicsObject(data.genreType, data.directorName, data.movieTitle, data.releaseYear,
+                                data.actorFirstName, data.actorLastname, data.releaseMonth);
+    }
+}
 
  
