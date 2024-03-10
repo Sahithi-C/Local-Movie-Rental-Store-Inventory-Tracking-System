29aef6000e05dedf48e0f7dd3872f7bd51e756f0
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 15:55:52 -0700

    Update comedy.cpp

--- comedy.cpp
+++ comedy.cpp
@@ -11,11 +11,22 @@ Comedy::Comedy() {
 
 }
 
+// Comedy(char genreType, const string &directorName, const string &movieTitle, int releaseYear)
+// Summary - initializes a Comedy object with specified values for genre type, director name,
+//           movie title, and release year.
+// Pre-conditions - The provided genreType should be a valid character representing the genre type.
+//                  The directorName and movieTitle should be non-empty strings.
+//                  releaseYear should be a valid integer representing the year of release.
+// Post-conditions - An instance of the Comedy class is created with the provided values.
 Comedy::Comedy(char genreType, const string &directorName, const string &movieTitle, int releaseYear) 
        : Movies(genreType, directorName), Content(movieTitle, releaseYear) {
 
 }
 
+// ~Comedy()
+// Summary - This is the destructor for the Comedy class.
+// Pre-conditions - None.
+// Post-conditions - Resources held by the Comedy object are released upon destruction.
 Comedy::~Comedy(){
     
 }
