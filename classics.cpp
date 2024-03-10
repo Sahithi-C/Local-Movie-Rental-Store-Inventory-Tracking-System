0854707f0883537e252d3979776d6a9c1b1c5622
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 15:50:34 -0700

    Update classics.cpp

--- classics.cpp
+++ classics.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+// Classics()
+// Summary - default constructor for the Classics class.
+
+// Pre-conditions - None.
+
+// Post-conditions - An instance of the Classics class is created with default values.
 Classics::Classics(){
 }
 
+// Classics(char genreType, const string &directorName, const string &movieTitle,
+//          int releaseYear, const string &actorFirstName, const string &actorLastname,
+//          int releaseMonth)
+// Summary - initializes a Classics object with specified values for genre type, director name,
+//           movie title, release year, actor's first and last name, and release month.
+
+// Pre-conditions - The provided genreType should be a valid character representing the genre type.
+//                  The directorName, movieTitle, actorFirstName, and actorLastName should be non-empty strings.
+//                  releaseYear and releaseMonth should be valid integers representing the year and month of release.
+
+// Post-conditions - An instance of the Classics class is created with the provided values.
 Classics::Classics(char genreType, const string &directorName,
                    const string &movieTitle, int releaseYear,
                    const string &actorFirstName, const string &actorLastname,
@@ -18,5 +35,11 @@ Classics::Classics(char genreType, const string &directorName,
     this->releaseMonth = releaseMonth;
 }
 
+// ~Classics()
+// Summary - destructor for the Classics class.
+
+// Pre-conditions - None.
+
+// Post-conditions - Resources held by the Classics object are released upon destruction.
 Classics::~Classics() {
 }
