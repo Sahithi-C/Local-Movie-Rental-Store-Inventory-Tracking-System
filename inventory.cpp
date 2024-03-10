8c9e51764ed70868fc2f4e47f2f2bc37090ea727
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 16:31:40 -0700

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -3,10 +3,23 @@
 
 using namespace std;
 
+// Inventory()
+// Summary - Default constructor for the Inventory class.
+// Pre-conditions - None.
+// Post-conditions - An instance of the Inventory class is created.
 Inventory::Inventory() {}
 
+// ~Inventory()
+// Summary - Destructor for the Inventory class.
+// Pre-conditions - None.
+// Post-conditions - Resources held by the Inventory object are released upon destruction.
 Inventory::~Inventory() {}
 
+// populateInventory(const list<InventoryData> &inventoryList)
+// Summary - This method populates the inventory with content based on the provided inventory list.
+// Pre-conditions - The inventoryList should contain valid InventoryData objects representing content to be added to the inventory.
+// Post-conditions - The inventory is populated with content based on the data provided in the inventoryList.
+//                 - Each item in the inventory is associated with a Media object representing its stock and content.
 void Inventory::populateInventory(const list<InventoryData> &inventoryList) {
 
     for(const auto& data: inventoryList) {
