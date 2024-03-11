2d51870be00debde2ac4c61889ac9b5c2520bbbe
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-11 12:07:30 -0700

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -30,11 +30,13 @@ class Inventory
 
     // Inventory()
     // Summary - Default constructor.
+    // Pre-conditions - None.
     // Post-conditions - Initializes an empty inventory object.
     Inventory();
 
     // ~Inventory()
     // Summary - Destructor.
+    // Pre-conditions - None.
     // Post-conditions - Clears any dynamically associated memory.
     virtual ~Inventory();
 
@@ -42,37 +44,44 @@ class Inventory
 
     // getMedia(mediaId)
     // Summary - Gets the Media object using the given mediaId.
-    //           mediaId is a unique string representing the mediaType and contentId
+    //           mediaId is a unique string representing the mediaType and contentId.
+    // Pre-conditions - None.
     // Post-conditions - Returns the Media item of the given mediaId.
     Media& getMedia(string mediaId);
 
     // getComedy(mediaId)
     // Summary - Gets the Media object representing copies of a Comedy Movie using the given mediaId.
-    //           mediaId is a unique string representing the mediaType and contentId
+    //           mediaId is a unique string representing the mediaType and contentId.
+    // Pre-conditions - None.
     // Post-conditions - Returns the Media item of the given mediaId.
     Media& getComedy(string mediaId);
 
     // getDrama(mediaId)
     // Summary - Gets the Media object representing copies of a Drama Movie using the given mediaId.
-    //           mediaId is a unique string representing the mediaType and contentId
+    //           mediaId is a unique string representing the mediaType and contentId.
+    // Pre-conditions - None.
     // Post-conditions - Returns the Media item of the given mediaId.
     Media& getDrama(string mediaId);
 
     // getClassics(mediaId)
     // Summary - Gets the Media object representing copies of a Classics Movie using the given mediaId.
-    //           mediaId is a unique string representing the mediaType and contentId
+    //           mediaId is a unique string representing the mediaType and contentId.
+    // Pre-conditions - None.
     // Post-conditions - Returns the Media item of the given mediaId.
     Media& getClassics(string mediaId);
 
     //---------------------------------------------------other member functions-----------------------------------------------------
 
     // populateInventory()
-    // Summary - Populates the inventory object with data from the parser.
-    // Post-conditions - The inventory is populated with valid media items.
+    // Summary - This method populates the inventory with content based on the provided inventory list.
+    // Pre-conditions - The inventoryList should contain valid InventoryData objects representing content to be added to the inventory.
+    // Post-conditions - The inventory is populated with content based on the data provided in the inventoryList.
+    //                 - Each item in the inventory is associated with a Media object representing its stock and content.
     void populateInventory(const list<InventoryData>& inventoryList);
 
-    // getSortedInventory()
+    // printSortedInventory()
     // Summary - Generates and returns a sorted list of all Media objects in the store.
+    // Pre-conditions - None.
     // Post-conditions - The return will be a sorted list of all Media objects in the store.
     ostream& printSortedInventory(ostream& out) const;
 
