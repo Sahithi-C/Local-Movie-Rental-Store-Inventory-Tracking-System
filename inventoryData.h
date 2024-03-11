0d033340b5077b4fc50788ca0f04c6f15f0b42ec
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 18:55:03 -0700

    Update inventoryData.h

--- inventoryData.h
+++ inventoryData.h
@@ -7,14 +7,14 @@ using namespace std;
 
 struct InventoryData
 {
-    char genreType;
-    int stock;
-    string directorName;
-    string movieTitle;
-    int releaseYear;
-    string actorFirstName;
-    string actorLastname;
-    int releaseMonth;
+    char genreType;         // Genre type of the content
+    int stock;              // Stock count of the content
+    string directorName;    // Name of the director
+    string movieTitle;      // Title of the movie
+    int releaseYear;        // Year of release
+    string actorFirstName;  // First name of the major actor
+    string actorLastname;   // Last name of the major actor
+    int releaseMonth;       // Month of release
 };
 
 #endif
