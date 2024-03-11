c153c4ba7fc39881043ad98b31890fb1e4f77023
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 19:33:48 -0700

    Update classics.cpp

--- classics.cpp
+++ classics.cpp
@@ -1,3 +1,15 @@
+//------------------------------------------------------classics.cpp-----------------------------------------------------------
+
+// Programmer Name Course Section Number : VG, Jonah Kolar, Sahithi Chimakurthi(CSS 502 A)
+
+// Creation Date : 03/10/2024
+
+// Date of Last Modification : 03/10/2024
+
+// Purpose : Implementing a Classics class representing a classic movie.This class inherits from Movie class. 
+
+// -------------------------------------------------------------------------------------------------------------------------
+
 #include<iostream>
 #include "classics.h"
 
