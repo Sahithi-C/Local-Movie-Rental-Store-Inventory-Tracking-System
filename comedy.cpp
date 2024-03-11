f6abcd20a316c844eac8838d1a3cf76cd2a743ff
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 20:00:26 -0700

    Update comedy.cpp

--- comedy.cpp
+++ comedy.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 // Comedy()
-// Summary - default constructor for the Comedy class.
+// Summary - Default constructor for the Comedy class.
 // Pre-conditions - None.
 // Post-conditions - An instance of the Comedy class is created with default values.
 Comedy::Comedy() {
@@ -24,7 +24,7 @@ Comedy::Comedy() {
 }
 
 // Comedy(char genreType, const string &directorName, const string &movieTitle, int releaseYear)
-// Summary - initializes a Comedy object with specified values for genre type, director name,
+// Summary - Initializes a Comedy object with specified values for genre type, director name,
 //           movie title, and release year.
 // Pre-conditions - The provided genreType should be a valid character representing the genre type.
 //                  The directorName and movieTitle should be non-empty strings.
