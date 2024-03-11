a7bab731e2add59b7cc0ef208ecc25eca1c3509d
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 20:04:56 -0700

    Update comedy.h

--- comedy.h
+++ comedy.h
@@ -29,6 +29,13 @@ class Comedy : public Movies  // Comedy class inherits from Movie class.
     Comedy();
 
     // Parameterized constructor
+    // Comedy(char genreType, const string &directorName, const string &movieTitle, int releaseYear)
+    // Summary - Initializes a Comedy object with specified values for genre type, director name,
+    //           movie title, and release year.
+    // Pre-conditions - The provided genreType should be a valid character representing the genre type.
+    //                  The directorName and movieTitle should be non-empty strings.
+    //                  releaseYear should be a valid integer representing the year of release.
+    // Post-conditions - An instance of the Comedy class is created with the provided values.
     Comedy(char genreType, const string& directorName, const string& movieTitle, int releaseYear);
 
     // Comedy(const Comedy& other)
@@ -48,7 +55,7 @@ class Comedy : public Movies  // Comedy class inherits from Movie class.
     // getMovieId() const
     // Summary - Gets the unique ID for a comedy movie.
     // Pre-conditions - There should be a valid movie in movies of drama genre type.
-    // Post-conditions - Returns the unique id for a drama movie.
+    // Post-conditions - Returns the unique ID for a drama movie.
     int getMovieId() const;
 
 };
