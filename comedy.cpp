55e98bed9d467d44e91c1b910b8889e71b466354
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 19:34:42 -0700

    Update comedy.cpp

--- comedy.cpp
+++ comedy.cpp
@@ -1,3 +1,15 @@
+//------------------------------------------------------Comedy.cpp-----------------------------------------------------------
+
+// Programmer Name Course Section Number : VG, Jonah Kolar, Sahithi Chimakurthi(CSS 502 A)
+
+// Creation Date : 03/10/2024
+
+// Date of Last Modification : 03/10/2024
+
+// Purpose : Implementing a Comedy class representing a funny movie.This class inherits from Movie class. 
+
+// -------------------------------------------------------------------------------------------------------------------------
+
 #include<iostream>
 #include "comedy.h"
 
