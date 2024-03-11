552d090dd9f3aa5475c94228f9db9b09bf657e63
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-10 19:47:07 -0700

    Update drama.cpp

--- drama.cpp
+++ drama.cpp
@@ -1,3 +1,15 @@
+//------------------------------------------------------drama.cpp-----------------------------------------------------------
+
+// Programmer Name Course Section Number : VG, Jonah Kolar, Sahithi Chimakurthi(CSS 502 A)
+
+// Creation Date : 03/10/2024
+
+// Date of Last Modification : 03/10/2024
+
+// Purpose : Implementing the Drama class representing a drama movie.This class inherits from Movie class. 
+
+// -------------------------------------------------------------------------------------------------------------------------
+
 #include<iostream>
 #include "drama.h"
 using namespace std;
