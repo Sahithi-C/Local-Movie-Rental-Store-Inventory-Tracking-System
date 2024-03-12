200320a8f00a1f1863e7ea8ee182192af46443e2
Sahithi-C <117063184+Sahithi-C@users.noreply.github.com>
2024-03-11 18:26:46 -0700

    Update movies.cpp

--- movies.cpp
+++ movies.cpp
@@ -13,6 +13,7 @@
 
 // Movies()
 // Summary - Default constructor.
+// Pre-conditions - None.
 // Post-conditions - Constructs a movie object.
 Movies::Movies()
     : Content() {
@@ -20,6 +21,11 @@ Movies::Movies()
     genreType = 'N';
 }
 
+// Movies(char genreType, const string &director, const string &movieTitle, int releaseYear)
+// Summary - Parameterized constructor for the Movies class.
+// Pre-conditions - None.
+// Post-conditions - A Movies object is initialized with the specified genre type, director, movie title, and release year.
+//                   The content (movie title and release year) is set through the Content constructor.
 Movies::Movies(char genreType, const string &director, const string &movieTitle, int releaseYear) 
     : Content(movieTitle, releaseYear) {
     this->genreType = genreType;
@@ -28,6 +34,7 @@ Movies::Movies(char genreType, const string &director, const string &movieTitle,
 
 // Movies(Movies& other)
 // Summary - Copy constructor.
+// Pre-conditions - The 'other' parameter must be a valid Movies object.
 // Post-conditions - constructs movie object as a copy of another movies object.
 Movies::Movies(const Movies& other)
     : Content(other) {
@@ -37,12 +44,14 @@ Movies::Movies(const Movies& other)
 
 // ~Movies()
 // Summary - Destructor.
+// Pre-conditions - None.
 // Post-conditions - cleans up any dynamically allocated memory.
 Movies::~Movies(){}
 
 //---------------------------------------------Setter methods--------------------------------------------------------------
 // setDirector(const string& director)
 // Summary - Sets the director of a movie.
+// Pre-conditions - The input string 'director' should not be empty.
 // Post-conditions - Director of a movie is set.
 //                   Returns true if the director is set.
 //                   false otherwise.
@@ -53,6 +62,7 @@ bool Movies::setDirector(const string& director){
 
 // setGenreType(char genre)
 // Summary - Sets the genre type for a movie.
+// Pre-conditions - The genreType must be a valid character representing the genre type of the movie.
 // Post-conditions - genreType is set for a movie.
 //                   Returns true if the genre is set.
 //                   false otherwise.
@@ -65,6 +75,7 @@ bool Movies::setGenreType(char genreType){
 
 // getGenreType() const
 // Summary - Gets the genre type of a movie.
+// Pre-conditions - None.
 // Post-conditions - Returns genre type of a movie.
 char Movies::getGenreType() const{
     return genreType;
@@ -72,6 +83,7 @@ char Movies::getGenreType() const{
 
 // getDirector() const
 // Summary - Gets the director of a movie.
+// Pre-conditions - None.
 // Post-conditions - Returns the firstName and lastName of the director of a movie.
 string Movies::getDirector() const{
     return director;
@@ -79,6 +91,7 @@ string Movies::getDirector() const{
 
 // getContentId() const
 // Summary - Abstract function to get a contentId, the unique identifier of this movie.
+// Pre-conditions - None.
 // Post-conditions - None.
 string Movies::getContentId() const{
     return Content::getContentId() + director + genreType;
@@ -89,10 +102,12 @@ string Movies::getContentId() const{
 
 // addMovie()
 // Summary - Adds a movie to the inventory.
+// Pre-conditions - None.
 // Post-conditions - A movie is added to the inventory.
 bool Movies::addMovie(){}
 
 // removeMovie()
 // Summary - Removes a movie from the inventory.
+// Pre-conditions - There sould be a movie existing in inventory to remove it.
 // Post-conditions - a movie is removed from the inventory.
 bool Movies::removeMovie(){}
